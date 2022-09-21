784c20264b16f3f368951a3390a3812eec223b9e
salutJuillet <salut.juilllet@gmail.com>
2022-09-21 15:42:49 +0900

    Screen 생성

---
+++ screens/ProfileScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProfielScreen = () => {
+  return (
+    <View>
+      <Text>ProfielScreen</Text>
+    </View>
+  )
+}
+
+export default ProfielScreen
