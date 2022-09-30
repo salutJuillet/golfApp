829f26a2ad6ed0b4a3972012e8de0e525ea13a41
salutJuillet <salut.juilllet@gmail.com>
2022-09-30 17:21:14 +0900

    HomeScreen.jsx 무한 슬라이드 변경

--- component/CarouselCourse.jsx
+++ component/CarouselCourse.jsx
@@ -1,136 +1,23 @@
-import { StyleSheet, Text, View, FlatList, Dimensions, ImageBackground } from 'react-native'
-import React, {useState, useEffect, useRef} from 'react'
-
+import { StyleSheet, Text, View, Dimensions, ImageBackground } from 'react-native'
+import React from 'react'
+import Carousel from 'react-native-reanimated-carousel'
+import { course } from '../tempDB/course';
 
 const width = Dimensions.get('window').width;
-let currentSlideIndex = 0;
-let intervalId;
-
-/* DB */
-import {course} from '../tempDB/course';
-// console.log(course);
 
 const CarouselCourse = () => {
 
-  const [dataToRender, setDataToRender] = useState([]);
-  const [visibleSlideIndex, setVisibleSlideIndex] = useState(0);
-  const [activeSlideIndex, setActiveSlideIndex]  = useState(0);
-
-  const onViewableItemsChanged = useRef(({viewableItems})=>{
-    currentSlideIndex = viewableItems[0]?.index || 0;
-    // console.log(currentSlideIndex);
-    setVisibleSlideIndex(currentSlideIndex);
-  });
-
-  const viewabilityConfig = useRef({
-    viewAreaCoveragePercentThreshold: 50
-  });
-
-  const flatList = useRef();
-
-  const handleScrollTo = (index) => {
-    flatList.current.scrollToIndex({animated:false, index});
-  }
-
-  const startSlider = () => {
-    if(currentSlideIndex <= dataToRender.length - 2) { //마지막 번호까지 갔을 때
-        intervalId = setInterval(()=>{
-            flatList.current.scrollToIndex({
-                animated: true,
-                index: currentSlideIndex + 1
-            })
-        }, 4000);
-    }else{
-        pauseSlider();
-    }
-  }
-
-  const pauseSlider = () => {
-    clearInterval(intervalId);
-  }
-
-  useEffect(()=>{
-    if(dataToRender.length && flatList.current && typeof flatList.current.scrollToIndex ==='function') {
-        startSlider();
-    }
-  }, [dataToRender.length]);
-
-  useEffect(()=>{
-    const newData = [[...course].pop(), ...course, [...course].shift()];
-    setDataToRender([...newData]);
-  }, [course.length]);
-
-  useEffect(()=>{
-    const length = dataToRender.length;
-    //첫 번째 리셋
-    if(visibleSlideIndex === length -1 && length) {
-        handleScrollTo(1);
-    }
-    //마지막 페이지 리셋
-    if(visibleSlideIndex === 0 && length) {
-        handleScrollTo(length - 2);
-    }
-    const lastSlide = currentSlideIndex === length - 1;
-    const firstSlide = currentSlideIndex === 0;
-
-    if(lastSlide && length) setActiveSlideIndex(0)
-    else if(firstSlide && length) setActiveSlideIndex(length - 2)
-    else setActiveSlideIndex(currentSlideIndex - 1)
-  }, [visibleSlideIndex])
-
   return (
-    <View style={[sty.container, {position:'relative'}]}>
-      <View 
-        style={{
-            flexDirection:'row',
-            alignItems:'center',
-            justifyContent:'flex-end',
-            position:'absolute',
-            right:0,
-            top:40,
-            zIndex:10
-        }}
-      >
-        <View
-          style={{
-            flexDirection:'row', alignItems:'center', justifyContent:'center', paddingHorizontal: 20, marginTop:-25
-          }}
-        >
-            {
-                course.map((item, index)=>(
-                    <View key={item.id}
-                          style={{
-                            width:12,
-                            height:12,
-                            borderRadius:6,
-                            borderWidth:2,
-                            marginRight:5,
-                            backgroundColor: activeSlideIndex === index ? '#000' : 'transparent'
-                          }}
-                    />
-                ))
-            }
-        </View>
-      </View>
-
-      <FlatList
-        ref={flatList}
-        data={dataToRender}
-        keyExtractor={(item, index)=>item.id + index}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        initialScrollIndex={1}
-        getItemLayout={(_, index)=>({
-            length: width,
-            offset: width * index,
-            index
-        })}
-        onViewableItemsChanged={onViewableItemsChanged.current}
-        viewabilityConfig={viewabilityConfig.current}
-        onScrollBeginDrag={pauseSlider}
-        onScrollEndDrag={startSlider}
-        renderItem={({item})=>(
+    <View>
+      <Carousel
+          loop
+          width={width}
+          height={width / 1.6}
+          autoPlay={true}
+          data={course}
+          scrollAnimationDuration={4000}
+          // onSnapToItem={(index) => console.log('current index:', index)}
+          renderItem={({item, index})=>(
             <ImageBackground 
                 source={item.img}
                 style={{
@@ -155,7 +42,7 @@ const CarouselCourse = () => {
                     {item.name}
                 </Text>
             </ImageBackground>
-        )}
+          )}
       />
     </View>
   )
@@ -163,11 +50,4 @@ const CarouselCourse = () => {
 
 export default CarouselCourse
 
-const sty = StyleSheet.create({
-    container:{
-        alignSelf:'center',
-        width,
-        marginBottom:30,
-        backgroundColor:'#f2f2f2'
-    }
-})
+const styles = StyleSheet.create({})
